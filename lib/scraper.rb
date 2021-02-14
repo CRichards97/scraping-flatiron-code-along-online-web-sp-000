a983e24b64ed57a3ba134a3abbec36e7b3c2fc4e
Clint Richards <richardsclint17@gmail.com>
2021-02-14 05:34:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,6 @@
+require 'nokogiri'
+require 'open-uri'
+require_relative './course.rb'
 class Scraper
   def get_page
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
